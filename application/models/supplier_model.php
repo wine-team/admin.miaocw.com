edac7b81dafaf939850647c49517314220e918d6
wenjoying <wenjoying@126.com>
2016-05-25 00:34:35 +0800

    供应商

--- application/models/supplier_model.php
+++ application/models/supplier_model.php
@@ -1,24 +1,22 @@
 <?php
-class Web_notice_model extends CI_Model{
+
+class Supplier_model extends CI_Model{
+	private $table = 'supplier';        
 	
-	private $table = 'Web_notice';        
-	
-	public function web_notice_list($page, $perpage, $search, $order='id DESC')
+	public function supplier_list($page, $perpage, $search, $order='supplier_id DESC')
 	{
-	    if(!empty($search['item'])) 
+	    if(!empty($search['item']))
 	    {
-	        $this->db->like('title', $search['item']);
-	        $this->db->or_like('notice_info', $search['item']);
+	        $this->db->like('supplier_name', $search['item']);
+	        $this->db->or_like('supplier_desc', $search['item']);
 	    }
 	    $this->db->order_by($order);
 	    if($perpage) $this->db->limit($perpage, $perpage*$page);
 	    return $this->db->get($this->table);
 	}
 	
-	public function findById($where, $order='id ASC', $limit=0)
+	public function findById($where)
 	{
-	    $this->db->order_by($order);
-	    if($limit) $this->db->limit($limit);
 	    return $this->db->get_where($this->table, $where);
 	}
 	
@@ -42,5 +40,5 @@ class Web_notice_model extends CI_Model{
 	
 }
 
-/* End of file Help_center_model.php */
-/* Location: ./application/models/Help_center_model.php */
+/* End of file Supplier_model.php */
+/* Location: ./application/models/Supplier_model.php */
---
+++ application/views/supplier/edit.php
@@ -0,0 +1,118 @@
+<?php $this->load->view('layout/header');?>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span12">
+            <h3 class="page-title">用户管理 <small>供应商管理</small></h3>
+            <?php echo breadcrumb(array('用户管理', 'supplier/grid'=>'供应商管理', '编辑供应商')); ?>
+        </div>
+    </div>
+    <div class="alert alert-error" style="display: none;">
+        <button data-dismiss="alert" class="close"></button>
+        <a href="javascript:;" class="glyphicons no-js remove_2"><i></i><p></p></a>
+    </div>
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="portlet box green">
+                <div class="portlet-title">
+                    <div class="caption"><i class="icon-plus-sign"></i>编辑供应商</div>
+                    <div class="tools">
+                        <a class="collapse" href="javascript:;"></a>
+                        <a class="remove" href="javascript:;"></a>
+                    </div>
+                </div>
+                <div class="portlet-body form">
+                    <form class="form-horizontal line-form supplier-form" action="<?php echo base_url('supplier/addPost') ?>" method="post" enctype ="multipart/form-data" >
+                        <input type="hidden" name="supplier_id" value="<?php echo $res->supplier_id;?>">
+                        <div class="control-group add-supplieruid-html">
+                            <label class="control-label"><em>* </em>用户id</label>
+                            <div class="controls">
+                                <input type="text" name="uid" value="<?php echo $res->uid;?>" class="m-wrap medium supplieruid tooltips number" data-original-title="双击可弹框选择供应商；直接输入‘用户名称或编号’可提示" data-trigger="hover">
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <label class="control-label"><em>* </em>供应商名称</label>
+                            <div class="controls">
+                                <input type="text" name="supplier_name" value="<?php echo $res->supplier_name;?>" class="m-wrap large required"/>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <label class="control-label">供应商简介</label>
+                            <div class="controls">
+                                <textarea class="textarea-multipart-edit" name="supplier_desc"><?php echo $res->supplier_desc;?></textarea>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <label class="control-label"><em>* </em>状态</label>
+                            <div class="controls">
+                                <label class="radio">
+                                	<input type="radio" class="required" name="is_check" value="1" <?php if($res->is_check==1) echo 'checked="checked"';?> /> 正常
+                                </label>
+                                <label class="radio">
+                                	<input type="radio" class="required" name="is_check" value="2" <?php if($res->is_check==2) echo 'checked="checked"';?>/>冻结
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-actions">
+                            <button class="btn green" type="submit"><i class="icon-ok"></i> 保存</button>
+                            <a href="<?php echo base_url('supplier/grid') ?>">
+                                <button class="btn" type="button">返回</button>
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->load->view('layout/footer');?>
+<?php $this->load->view('user/addSupplierUid/ajaxGetUser');?>
+<script type="text/javascript" >
+$('form.supplier-form').submit(function(){
+    return false;
+}).validate({
+    rules: {
+    	uid: {
+            required: true,
+            remote: {
+                url:hostUrl()+'/supplier/validateUser',
+                type: 'post',
+                dataType: 'json',
+                data: {
+                	uid:function(json) {
+                        return $('input[name=uid]').val();
+                    },
+                    supplier_id:function(json) {
+                        return $('input[name=supplier_id]').val();
+                    }
+                }
+            }
+        },
+    },
+    messages: {
+    	uid: {
+    		required:'请输入用户id',
+            remote: '此用户已经是供应商了'
+        },
+    },
+    submitHandler: function(f) {
+        $.ajax({
+            type: 'post',
+            async: true,
+            dataType : 'json',
+            url: hostUrl() + '/supplier/editPost',
+            data: $('form.supplier-form').serialize(),
+            success: function(data) {
+                if (data.status) {
+                    $('.alert-error').hide();
+                    window.location.href = data.messages;
+                } else {
+                    $('.alert-error').show();
+                    $('.remove_2 p').html(data.messages);
+                    var body = (window.opera) ? (document.compatMode == 'CSS1Compat' ? $('html') : $('body')) : $('html,body');
+                    body.animate({scrollTop: $('.page-container').offset().top}, 1000);
+                }
+            }
+        });
+    }
+});
+</script>
