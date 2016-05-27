e3378ec67f33efae1e70f114151b6eae66fb6c72
wenjoying <wenjoying@126.com>
2016-05-27 22:11:13 +0800

    新增mall_goods_attr

---
+++ application/models/mall_attribute_model.php
@@ -0,0 +1,42 @@
+<?php
+class Mall_attribute_model extends CI_Model{
+	private $table = 'mall_attribute';        
+	
+	public function mall_attribute_list($page, $perpage, $search, $order='attr_id DESC')
+	{
+	    if(!empty($search['item']))
+	    {
+	        $this->db->like('attr_name', $search['item']);
+	        $this->db->or_like('attr_values', $search['item']);
+	    }
+	    $this->db->order_by($order);
+	    if($perpage) $this->db->limit($perpage, $perpage*$page);
+	    return $this->db->get($this->table);
+	}
+	
+	public function findById($where)
+	{
+	    return $this->db->get_where($this->table, $where);
+	}
+	
+	public function insert($data) 
+	{
+	    $this->db->insert($this->table, $data);
+	    return $this->db->insert_id();
+	} 
+	
+	public function update($where, $data)  
+	{
+	    $this->db->update($this->table, $data, $where);
+	    return $this->db->affected_rows();
+	}
+	
+	public function delete($where)  
+	{
+	    $this->db->delete($this->table, $where);
+	    return $this->db->affected_rows();
+	}
+	
+}
+/* End of file Mall_attribute_model.php */
+/* Location: ./application/models/Mall_attribute_model.php */
---
+++ application/views/mall_goods_attr/grid.php
@@ -0,0 +1,70 @@
+<?php $this->load->view('layout/header');?>
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span12">
+            <h3 class="page-title">商品管理 <small>商品属性</small></h3>
+            <?php echo breadcrumb(array('商品管理 ', 'supplier/grid'=>'商品属性',)); ?>
+        </div>
+    </div>
+    <?php echo execute_alert_message() ?>
+    <div class="row-fluid">
+        <div class="span12">
+            
+            <div class="portlet box green">
+                <div class="portlet-title">
+                    <div class="caption"><i class="icon-reorder"></i>列表</div>
+                    <div class="tools">
+                        <a class="collapse" href="javascript:;"></a>
+                        <a class="remove" href="javascript:;"></a>
+                    </div>
+                </div>
+                <div class="portlet-body flip-scroll">
+                    <div class="dataTables_wrapper form-inline">
+                        <div class="clearfix">
+                            <a href="<?php echo base_url('mall_goods_attr/add/'.$goods_id) ?>" class="add-button-link">
+                                <div class="btn-group">
+                                    <button class="btn green"><i class="icon-plus"></i> 添加</button>
+                                </div>
+                            </a>
+                        </div>
+                        <?php if (count($res) > 0) :?>
+                        <table class="table table-striped table-bordered table-hover" id="sample_1">
+                            <thead class="flip-content">
+                                <tr>
+                                    <th><input type="checkbox" class="group-checkable" data-set="#sample_1 .checkboxes"></th>
+                                    <th>编号</th>
+                                    <th>商品编号</th>
+                                    <th>属性id(关联商品属性)</th>
+                                    <th>属性值</th>
+                                    <th>价格</th>
+                                    <th>排序</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($res as $r) : ?>
+                                <tr>
+                                    <td width="15"><input type="checkbox" class="checkboxes" value="1" ></td>
+                                    <td><?php echo $r->goods_attr_id;?></td>
+                                    <td><?php echo $r->goods_id;?></td>
+                                    <td><?php echo $r->attr_id;?></td>
+                                    <td><?php echo $r->attr_value;?></td>
+                                    <td><?php echo $r->attr_price;?></td>
+                                    <td width="145">
+                                        <a class="btn mini green" href="<?php echo base_url('mall_goods_attr/edit/'.$r->goods_attr_id); ?>"><i class="icon-edit"></i> 编辑</a>
+                                        <a class="btn mini green" href="<?php echo base_url('mall_goods_attr/delete/'.$r->goods_attr_id.'?goods_id='.$r->goods_id); ?>" onclick="return confirm('确定要删除？')"><i class="icon-trash"></i> 删除</a>
+                                    </td>
+                                </tr>
+                                <?php endforeach;?>
+                            </tbody>
+                        </table>
+                        
+                        <?php else: ?>
+                            <div class="alert"><p>未找到数据。<p></div>
+                        <?php endif ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->load->view('layout/footer');?>
