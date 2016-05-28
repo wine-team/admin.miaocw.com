485f3345dad83787fdddc290c2f879577f66998d
604942578 <604942578@qq.com>
2016-05-28 20:53:46 +0800

    区块广告的设置 和添加   头部的隐藏去掉

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -1,4 +1,4 @@
-<?php $this->load->view('layout/head');?>
+ <?php $this->load->view('layout/head');?>
 <body class="page-header-fixed"><?php //page-sidebar-closed 可默认关闭sidebar //page-sidebar-fixed 默认伸缩?>
     <?php $this->load->view('layout/headerlinks');?>
     <div class="page-container">
