98fb6a81ede4e2e2320a56beeaa79d903a3a3c51
wenjoying <wenjoying@126.com>
2016-06-01 00:25:11 +0800

    修改

--- application/views/mall_category/grid.php
+++ application/views/mall_category/grid.php
@@ -34,6 +34,7 @@
                                     <th >编号</th>
                                     <th >分类名称</th>
                                     <th >上级ID</th>
+                                    <th >分类等级</th>
                                     <th >显示状态</th>
                                     <th >排序</th>
                                     <th >关键字</th>
@@ -50,6 +51,7 @@
                                     <td style="table-layout:fixed;width:50px;"><?php echo $r->cat_id;?></td>
                                     <td style="table-layout:fixed;width:120px;"><?php echo $r->cat_name;?></td>
                                     <td style="table-layout:fixed;width:50px;"><?php echo $r->parent_id;?></td>
+                                    <td style="table-layout:fixed;width:80px;"><?php if($r->cat_type==1){echo '一级分类';}else{echo '二级分类';} ;?></td>
                                     <td style="table-layout:fixed;width:80px;"><?php if($r->is_show==1)echo '是';?></td>
                                     <td style="table-layout:fixed;width:50px;"><?php echo $r->sort_order;?></td>
                                     <td style="table-layout:fixed;width:100px;"><?php echo $r->keyword;?></td>
@@ -60,7 +62,7 @@
                                         <a class="btn mini green" href="<?php echo base_url('mall_category/delete/'.$r->cat_id); ?>" onclick="return confirm('确定要删除？')"><i class="icon-trash"></i> 删除</a>
                                     </td>
                                 </tr>
-                                <tr class="p_cat1" style="display:none;"><td colspan="10"><table class="table-striped table-bordered table-hover">
+                                <tr class="p_cat1" style="display:none;"><td colspan="11"><table class="table-striped table-bordered table-hover">
                                     <?php foreach ($res as $r1) : ?>
                                     <?php if($r->cat_id == $r1->parent_id) :?>
                                     <tr class="p_cat2">
@@ -68,6 +70,7 @@
                                         <td style="table-layout:fixed;width:50px;"><?php echo $r1->cat_id;?></td>
                                         <td style="table-layout:fixed;width:120px;"><?php echo $r1->cat_name;?></td>
                                         <td style="table-layout:fixed;width:50px;"><?php echo $r1->parent_id;?></td>
+                                        <td style="table-layout:fixed;width:80px;"><?php if($r1->cat_type==1){echo '一级分类';}else{echo '二级分类';} ;?></td>
                                         <td style="table-layout:fixed;width:80px;"><?php if($r1->is_show==1)echo '是';?></td>
                                         <td style="table-layout:fixed;width:50px;"><?php echo $r1->sort_order;?></td>
                                         <td style="table-layout:fixed;width:100px;"><?php echo $r1->keyword;?></td>
@@ -85,6 +88,7 @@
                                             <td style="table-layout:fixed;width:50px;"><?php echo $r2->cat_id;?></td>
                                             <td style="table-layout:fixed;width:120px;"><?php echo $r2->cat_name;?></td>
                                             <td style="table-layout:fixed;width:50px;"><?php echo $r2->parent_id;?></td>
+                                            <td style="table-layout:fixed;width:80px;"><?php if($r2->cat_type==1){echo '一级分类';}else{echo '二级分类';} ;?></td>
                                             <td style="table-layout:fixed;width:80px;"><?php if($r2->is_show==1)echo '是';?></td>
                                             <td style="table-layout:fixed;width:50px;"><?php echo $r2->sort_order;?></td>
                                             <td style="table-layout:fixed;width:100px;"><?php echo $r2->keyword;?></td>
