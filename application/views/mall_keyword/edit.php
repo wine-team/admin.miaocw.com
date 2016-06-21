367893c59d41831868ef757e507780a1bd51e82f
Rabin <zhangkunch@qq.com>
2016-06-21 21:40:33 +0800

    修改keyword关键词

--- application/views/mall_keyword/edit.php
+++ application/views/mall_keyword/edit.php
@@ -3,7 +3,7 @@
     <div class="row-fluid">
         <div class="span12">
             <h3 class="page-title">热门搜索<small>搜索列表</small></h3>
-            <?php echo breadcrumb(array('热门搜索', 'keyword_search/grid'=>'搜索列表', '编辑热门排序')); ?>
+            <?php echo breadcrumb(array('热门搜索', 'mall_keyword/grid'=>'搜索列表', '编辑热门排序')); ?>
         </div>
     </div>
     <?php echo execute_alert_message() ?>
@@ -18,7 +18,7 @@
                     </div>
                 </div>
                 <div class="portlet-body form">
-                    <form class="form-horizontal line-form" action="<?php echo base_url('keyword_search/editPost') ?>" method="post" enctype="multipart/form-data">
+                    <form class="form-horizontal line-form" action="<?php echo base_url('mall_keyword/editPost') ?>" method="post" enctype="multipart/form-data">
                         
                         <input type="hidden" name="id" value="<?php echo $res->id;?>">
                         
@@ -32,20 +32,20 @@
                         <div class="control-group">
                             <label class="control-label">搜索次数</label>
                             <div class="controls">
-                                <input type="text" class="m-wrap large" readonly value="<?php echo $res->number;?>"/> 
+                                <input type="text" value="<?php echo $res->number;?>" class="m-wrap large" readonly="readonly"/>
                             </div>
                         </div>
                         
                         <div class="control-group">
                             <label class="control-label"><em>* </em>排序</label>
                             <div class="controls">
-                                <input type="number" class="m-wrap large required" name="sort" maxlength=3 value="<?php echo $res->sort;?>"/> 
+                                <input type="number" name="sort" value="<?php echo $res->sort;?>" maxlength=3 class="m-wrap large required"/>
                             </div>
                         </div>
                         
                         <div class="form-actions">
                             <button class="btn green" type="submit"><i class="icon-ok"></i> 保存</button>
-                            <a href="<?php echo base_url('mall_address/grid') ?>">
+                            <a href="<?php echo base_url('mall_keyword/grid') ?>">
                                 <button class="btn" type="button">返回</button>
                             </a>
                         </div>
