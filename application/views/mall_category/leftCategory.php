97f1d74b7e9e6c04c90a5e1170f5a4e80cd60922
Rabin <zhangkunch@qq.com>
2016-07-14 22:06:38 +0800

    商品分类

--- application/views/mall_category/leftCategory.php
+++ application/views/mall_category/leftCategory.php
@@ -1,12 +1,6 @@
 <p><a href="#" class="btn red mini">添加主目录</a></p>
 <p><a href="#" class="btn blue mini">添加子目录</a></p>
-<?php if (!empty($categorys)) : ?>
-    <div class="margin-bottom-10">
-        <button type="button" class="btn mini" id="tree_1_collapse">Expand All</button>
-        <button type="button" class="btn mini" id="tree_1_expand">Collapse All</button>
-    </div>
-    <?php echo getCategoryHtml($categorys); ?>
-<?php endif;?>
+<?php echo getCategoryHtml($categorys); ?>
 <script type="text/javascript">
     jQuery(document).ready(function($) {
         $("#tree_1").on('click', '[data-toggle=branch]', function (event) {
@@ -22,23 +16,15 @@
                         if (json.status) {
                             $('#attribute-responsive').html(json.html);
                         }
-                        $this.removeClass('closed').children('i').removeClass('icon-folder-close').addClass('icon-folder-open').parent('a.tree-toggle').next('ul .branch').addClass('in');
+                        $this.removeClass('closed').next('ul .branch').addClass('in');
                     }
                 });
             } else {
-                $this.addClass('closed').children('i').removeClass('icon-folder-open').addClass('icon-folder-close').parent('a.tree-toggle').next('ul .branch').removeClass('in');
+                if ($this.next('ul .branch').size() > 0) {
+                    $this.addClass('closed').next('ul .branch').removeClass('in');
+                }
             }
             event.preventDefault();
-        });
-
-        $('#tree_1_collapse').click(function () {
-            $('.tree-toggle', $('#tree_1 > li > ul')).addClass("closed");
-            $('.branch', $('#tree_1 > li > ul')).removeClass("in");
-        });
-
-        $('#tree_1_expand').click(function () {
-            $('.tree-toggle', $('#tree_1 > li > ul')).removeClass("closed");
-            $('.branch', $('#tree_1 > li > ul')).addClass("in");
         });
     });
 </script>
