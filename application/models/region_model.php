41d0cfd8122924d17a5b5a5134f1d0ebf5eff05f
604942578 <604942578@qq.com>
2016-07-26 23:14:37 +0800

    region

--- application/models/region_model.php
+++ application/models/region_model.php
@@ -12,4 +12,15 @@ class Region_model extends CI_Model
         }
         return array();
     }
+    
+    /**
+     * 通过数组IDs获取多个地区
+     * @param unknown $regionids
+     */
+    public function getByRegionIds($regionids=array())
+    {
+    	$this->db->where_in('region_id', $regionids);
+    	$this->db->order_by('region_id', 'ASC');
+    	return $this->db->get($this->table);
+    }
 }
