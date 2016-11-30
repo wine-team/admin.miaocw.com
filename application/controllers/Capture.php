98c7b2e0a26bed19bcedf3a277cb969958cf6d32
Rabin <zhangkunch@qq.com>
2016-11-30 23:52:55 +0800

    抓取产品

--- application/controllers/Capture.php
+++ application/controllers/Capture.php
@@ -1,9 +1,9 @@
 <?php
-class Test extends MJ_Controller
+class Capture extends MJ_Controller
 {
     public function _init()
     {
-        header("Content-type:text/html;charset=utf-8");
+        header("Content-type:text/html;charset=gb2312");
         $this->load->model('region_model', 'region');
         $this->load->model('mall_goods_base_model', 'mall_goods_base');
         $this->load->model('mall_attribute_set_model','mall_attribute_set');
@@ -12,40 +12,48 @@ class Test extends MJ_Controller
         $this->load->model('mall_category_model', 'mall_category');
     }
 
-    public function index()
+    public function test()
+    {
+        require_once APPPATH.'libraries/phpQuery.php';
+        phpQuery::newDocumentFile('http://www.taohv.cn/goods-3682.html');
+
+        pr(pq('#wrap_con li:eq(1)')->html());exit;
+    }
+
+    public function women()
     {
         require_once APPPATH.'libraries/phpQuery.php';
         $productUrl = array();
         for ($i=1; $i<=12; $i++) {
             phpQuery::newDocumentFile('http://www.taohv.cn/category.php?top_cat_id=2&page='.$i);
             $items = pq('.plist ul li');
-            foreach ($items as $item) {
-                $productUrl[] = 'http://www.taohv.cn'.pq($item)->find('.ptupian a')->attr('href');
+            foreach ($items as $key=>$item) {
+                $productUrl[$key]['provide_price'] = trim(pq($item)->find('.pxinxi .xinxileft .xxjiage')->text(), '￥');
+                $productUrl[$key]['url'] = 'http://www.taohv.cn'.pq($item)->find('.ptupian a')->attr('href');
             }
             break;
         }
         $goodsData = array();
         if (!empty($productUrl)) {
             $item = array();
-            foreach ($productUrl as $url) {
-                phpQuery::newDocumentFile($url);
-                pr($url);
-                var_dump(pq('.cpinfo1 .salesName').html());exit;
-                $item['goods_name']           = pq('.cpinfo1 .title .salesName').next().text();
-                $item['goods_sku']            = 'MCW_'.pq('#goods_id').val();
+            foreach ($productUrl as $value) {
+                phpQuery::newDocumentFile($value['url']);
+                pr($value);
+                $item['goods_name']           = pq('#wrap_con ul.title li:eq(1)')->html();
+                $item['goods_sku']            = 'MCW'.pq('#wrap_con #goods_id')->val();
                 $item['from_id']              = 4; //商品来源
                 $item['brand_id']             = 0;
                 $item['goods_weight']         = 100;
-                $item['market_price']         = trim(pq('.cpjiage .shijia em').text(), '￥');
-                $provide_price = pq('.cpjiage u.ajax_price').text();
-                $item['shop_price']           = $this->rePrice($provide_price);
-                $item['provide_price']        = $provide_price;
+                $item['market_price']         = trim(pq('.cpjiage .shijia em')->text(), '￥');
+                $item['shop_price']           = $this->rePrice($value['provide_price']);
+                $item['provide_price']        = $value['provide_price'];
                 $item['promote_start_date']   = 0;
                 $item['promote_end_date']     = 0;
                 $item['attr_set_id']          = 5;
-                $item['goods_desc']           = pq('.brands_con .brands_middle').html();
-                $item['wap_goods_desc']       = pq('.brands_con .brands_middle').html();
-                $item['goods_note']           = $url;
+                $item['goods_brief']          = pq('#wrap_con ul.title li:eq(0)')->html();
+                $item['goods_desc']           = trim(pq('.brands_con .brands_middle')->html(), ' ');
+                $item['wap_goods_desc']       = trim(pq('.brands_con .brands_middle')->html(), ' ');
+                $item['goods_note']           = $value['url'];
                 $item['attr_spec']            = '';
                 $item['attr_value']           = '';
                 $item['goods_img']            = '';
@@ -71,7 +79,7 @@ class Test extends MJ_Controller
                 //$item['sort_order']           = 50;
                 $item['created_at']           = date('Y-m-d H:i:s');
                 $item['updated_at']           = date('Y-m-d H:i:s');
-                var_dump($item);exit;
+                pr($item);exit;
             }
         }
     }
