4b968cc869ad252b8591f35639399e3677e073e5
Rabin <zhangkunch@qq.com>
2016-12-13 23:31:09 +0800

    抓取产品

--- application/controllers/Capture.php
+++ application/controllers/Capture.php
@@ -34,118 +34,152 @@ class Capture extends MJ_Controller
                 echo $item->goods_id.'更新失败<br />';
             }
         }
+        
     }
 
     public function women()
     {
         require_once APPPATH.'libraries/phpQuery.php';
         $productUrl = array();
-        for ($i=1; $i<=12; $i++) {
+        for ($i=3; $i<=12; $i++) {
             phpQuery::newDocumentFile('http://www.taohv.cn/category.php?top_cat_id=2&page='.$i);
             $items = pq('.plist ul li');
             foreach ($items as $key=>$item) {
-                $productUrl[$key]['provide_price'] = preg_replace('/[^\.0123456789]/s', '', pq($item)->find('.pxinxi .xinxileft .xxjiage')->text());
-                $productUrl[$key]['url'] = 'http://www.taohv.cn'.pq($item)->find('.ptupian a')->attr('href');
+                $productUrl[$i][$key]['provide_price'] = preg_replace('/[^\.0123456789]/s', '', pq($item)->find('.pxinxi .xinxileft .xxjiage')->text());
+                $productUrl[$i][$key]['url'] = 'http://www.taohv.cn'.pq($item)->find('.ptupian a')->attr('href');
             }
-            break;
         }
         $goodsData = array();
         if (!empty($productUrl)) {
             $item = array();
-            foreach ($productUrl as $value) {
-                phpQuery::newDocumentFile($value['url']);
-                $goods_id = pq('#wrap_con #goods_id')->val();
-                $goods_sku = 'THW'.$goods_id;
-                if (!$goods_id) {
-                    continue;
-                }
-                //获取产品属性
-                $attrValues = pq('#wrap_con .basic_con dl');
-                $attrItem = array();
-                foreach ($attrValues as $k=>$v) {
-                    $attrItem['group_name'] = '女性用品-主体';
-                    $attrItem['group_value'][$k]['attr_name'] = trim(pq($v)->find('dt')->text(), '：');
-                    $attrItem['group_value'][$k]['attr_value'] = pq($v)->find('dd')->text();
-                }
-                $attr_value = array($attrItem);
-
-                //获取产品图片
-                $goodsimgs = pq('#wrap_con .mat_js_img dd');
-                $imgItem = array();
-                foreach ($goodsimgs as $kk=>$vv) {
-                    $imgItem[] = str_replace(array('changeImage(\'', '\');return false;'), array('', ''), pq($vv)->find('a')->attr('onclick'));
-                }
-                $goods_img = implode('|', $imgItem);
-
-                $item['goods_name']           = mb_convert_encoding(pq('#wrap_con ul.title li:eq(1)')->html(), 'UTF-8', 'GBK');
-                $item['goods_sku']            = $goods_sku;
-                $item['from_id']              = 4; //商品来源
-                //$item['brand_id']             = 0;
-                $item['goods_weight']         = 100;
-                $item['market_price']         = trim(pq('.cpjiage .shijia em')->text(), '￥');
-                $item['shop_price']           = $this->rePrice($value['provide_price']);
-                $item['provide_price']        = $value['provide_price'];
-                $item['promote_start_date']   = 0;
-                $item['promote_end_date']     = 0;
-                $item['attr_set_id']          = 5;
-                $item['goods_brief']          = mb_convert_encoding(pq('#wrap_con ul.title li:eq(0)')->html(), 'UTF-8', 'GBK');
-                $item['goods_desc']           = trim(pq('.brands_con .brands_middle')->html(), ' ');
-                $item['wap_goods_desc']       = trim(pq('.brands_con .brands_middle')->html(), ' ');
-                $item['goods_note']           = $value['url'];
-                $item['attr_spec']            = array();
-                $item['attr_value']           = $attr_value;
-                $item['goods_img']            = '';
-                $item['extension_code']       = 'simple';
-                $item['is_on_sale']           = 1;
-                $item['is_check']             = 2;
-                $item['in_stock']             = 100;
-                $item['booking_limit']        = 0;
-                $item['limit_num']            = 0;
-                $item['minus_stock']          = 1;
-                $item['integral']             = 0;
-                $item['supplier_id']          = 15;
-                $item['freight_id']           = 0;
-                $item['freight_cost']         = 0;
-                $item['province_id']          = 12;
-                $item['city_id']              = 123;
-                $item['district_id']          = 1363;
-                $item['address']              = '浙江省 杭州市 下城区 新天地跨贸小镇';
-                $item['auto_cancel']          = 720;
-                //$item['sale_count']           = 0;
-                //$item['review_count']         = 0;
-                //$item['tour_count']           = 0;
-                //$item['sort_order']           = 50;
-                //$item['created_at']           = date('Y-m-d H:i:s');
-                //$item['updated_at']           = date('Y-m-d H:i:s');
-
-                $result = $this->mall_goods_base->findByGoodsSku($goods_sku);
-                if ($result->num_rows() > 0) {
-                    $mallGoodsBase = $result->row(0);
-                    $goods_id = $mallGoodsBase->goods_id;
-                    $item['goods_id'] = $goods_id;
-                    $item['goods_note'] = $value['url'].'===='.$goods_sku;
-                    $item['attr_value'] = $value['attr_value'];
-                    if (empty($mallGoodsBase->goods_img)) {
-                        $item['goods_img'] = $goods_img;
+            foreach ($productUrl as $valueArray) {
+                foreach ($valueArray as $value) {
+                    phpQuery::newDocumentFile($value['url']);
+                    $goods_id = pq('#wrap_con #goods_id')->val();
+                    $goods_sku = 'THW' . $goods_id;
+                    if (!$goods_id) {
+                        continue;
                     }
-                    $update = $this->mall_goods_base->updateCopy($item);
-                    $note = '产品ID（'.$goods_id.'）更新';
-                } else {
-                    $item['is_check'] = 1;
-                    $item['goods_img'] = $goods_img;
-                    $goodsId = $this->mall_goods_base->insert($item);
-                    $isInsert = $this->mall_category_product->insertBatchByGoodsId($goodsId, array(2, 13, 14, 15, 16, 17, 18, 19, 20));
-                    $note = '产品ID（'.$goods_id.'）添加';
-                }
+                    //获取产品属性
+                    $attrValues = pq('#wrap_con .basic_con dl');
+                    $attrItem = array();
+                    foreach ($attrValues as $k => $v) {
+                        $attrItem['group_name'] = '女性用品-主体';
+                        $attrItem['group_value'][$k]['attr_name'] = trim(
+                            pq($v)->find('dt')->text(), '：'
+                        );
+                        $attrItem['group_value'][$k]['attr_value'] = pq($v)
+                            ->find('dd')->text();
+                    }
+                    $attr_value = array($attrItem);
 
-                if ((isset($update) && $update) || (isset($goods_id, $isInsert) && $goods_id && $isInsert)) {
-                    echo $goods_id.'成功<br />';
-                } else {
-                    echo $goods_id.'失败<br />';
-                }
-                if ( $goods_sku != 'THW3682' && $goods_sku != 'THW4772' && $goods_sku != 'THW1153' && $goods_sku != 'THW2979') {
-                    pr($item);
-                    exit('成功'.$goods_sku);
+                    //获取产品图片
+                    $goodsimgs = pq('#wrap_con .mat_js_img dd');
+                    $imgItem = array();
+                    foreach ($goodsimgs as $kk => $vv) {
+                        $imgItem[] = str_replace(
+                            array('changeImage(\'', '\');return false;'),
+                            array('', ''), pq($vv)->find('a')->attr('onclick')
+                        );
+                    }
+                    $goods_img = implode('|', $imgItem);
+
+                    $item['goods_name'] = mb_convert_encoding(
+                        pq('#wrap_con ul.title li:eq(1)')->html(), 'UTF-8',
+                        'GBK'
+                    );
+                    $item['goods_sku'] = $goods_sku;
+                    $item['from_id'] = 4; //商品来源
+                    //$item['brand_id']             = 0;
+                    $item['goods_weight'] = 100;
+                    $item['market_price'] = trim(
+                        pq('.cpjiage .shijia em')->text(), '￥'
+                    );
+                    $item['shop_price'] = $this->rePrice(
+                        $value['provide_price']
+                    );
+                    $item['provide_price'] = $value['provide_price'];
+                    $item['promote_start_date'] = 0;
+                    $item['promote_end_date'] = 0;
+                    $item['attr_set_id'] = 5;
+                    $item['goods_brief'] = mb_convert_encoding(
+                        pq('#wrap_con ul.title li:eq(0)')->html(), 'UTF-8',
+                        'GBK'
+                    );
+                    $item['goods_desc'] = mb_convert_encoding(
+                        trim(pq('.brands_con .brands_middle')->html(), ' '),
+                        'UTF-8', 'GBK'
+                    );
+                    $item['wap_goods_desc'] = mb_convert_encoding(
+                        trim(pq('.brands_con .brands_middle')->html(), ' '),
+                        'UTF-8', 'GBK'
+                    );
+                    $item['goods_note'] = $value['url'];
+                    $item['attr_spec'] = array();
+                    $item['attr_value'] = $attr_value;
+                    $item['goods_img'] = '';
+                    $item['extension_code'] = 'simple';
+                    $item['is_on_sale'] = 1;
+                    $item['is_check'] = 2;
+                    $item['in_stock'] = 100;
+                    $item['booking_limit'] = 0;
+                    $item['limit_num'] = 0;
+                    $item['minus_stock'] = 1;
+                    $item['integral'] = 0;
+                    $item['supplier_id'] = 15;
+                    $item['freight_id'] = 0;
+                    $item['freight_cost'] = 0;
+                    $item['province_id'] = 12;
+                    $item['city_id'] = 123;
+                    $item['district_id'] = 1363;
+                    $item['address'] = '浙江省 杭州市 下城区 新天地跨贸小镇';
+                    $item['auto_cancel'] = 720;
+                    //$item['sale_count']           = 0;
+                    //$item['review_count']         = 0;
+                    //$item['tour_count']           = 0;
+                    //$item['sort_order']           = 50;
+                    //$item['created_at']           = date('Y-m-d H:i:s');
+                    //$item['updated_at']           = date('Y-m-d H:i:s');
+
+                    $result = $this->mall_goods_base->findByGoodsSku(
+                        $goods_sku
+                    );
+                    if ($result->num_rows() > 0) {
+                        $mallGoodsBase = $result->row(0);
+                        $goods_id = $mallGoodsBase->goods_id;
+                        $item['goods_id'] = $goods_id;
+                        $item['goods_note'] = $value['url'] . '===='
+                            . $goods_sku;
+                        $item['attr_value'] = $value['attr_value'];
+                        if (empty($mallGoodsBase->goods_img)) {
+                            $item['goods_img'] = $goods_img;
+                        }
+                        $update = $this->mall_goods_base->updateCopy($item);
+                        $note = '产品ID（' . $goods_id . '）更新';
+                    } else {
+                        $item['is_check'] = 1;
+                        $item['goods_img'] = $goods_img;
+                        $goodsId = $this->mall_goods_base->insert($item);
+                        $isInsert
+                            = $this->mall_category_product->insertBatchByGoodsId(
+                            $goodsId, array(2, 13, 14, 15, 16, 17, 18, 19, 20)
+                        );
+                        $note = '产品ID（' . $goods_id . '）添加';
+                    }
+
+                    if ((isset($update) && $update)
+                        || (isset($goods_id, $isInsert) && $goods_id
+                            && $isInsert)
+                    ) {
+                        echo $note . '成功<br />';
+                    } else {
+                        echo $note . '失败<br />';
+                    }
+                    /*
+                    if ( $goods_sku != 'THW3682' && $goods_sku != 'THW4772' && $goods_sku != 'THW1153' && $goods_sku != 'THW2979') {
+                        pr($item);
+                        exit('成功'.$goods_sku);
+                    }*/
                 }
             }
         }
