36f3f4145fe757f8edd927dd8d8c3e73c5167e6e
Rabin <zhangkunch@qq.com>
2017-07-16 14:04:50 +0800

    用户类型修改

--- application/config/constants.php
+++ application/config/constants.php
@@ -26,7 +26,6 @@ define('DIR_WRITE_MODE', 0777);
 | These modes are used when working with fopen()/popen()
 |
 */
-
 define('FOPEN_READ',							'rb');
 define('FOPEN_READ_WRITE',						'r+b');
 define('FOPEN_WRITE_CREATE_DESTRUCTIVE',		'wb'); // truncates existing file data, use with care
@@ -37,18 +36,15 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 //用户类型定义    范围[1-2147483647] 依赖LEVEL的前32位和php int最大值    2147483647 matrix的X轴
-/*供应系统用户*/
-define('UTID_PROVIDER',   1);    //供货商
-define('UTID_PROSECOND',  2);    //二级供应商
-define('UTID_PURCHASER',  4);    //采购商
-
-/*公司用户系统*/
-define('UTID_ZB',         8);    //公司总部
-define('UTID_MANAGER_L1', 16);   //一级代理 收一级代理费--省代
-define('UTID_MANAGER_L2', 32);   //二级代理 收二级代理费 --市代
-define('UTID_GROWTH',     64);   //发展商    收发展费
-define('UTID_SELLER',     128);  //分销商    收销售费
-define('UTID_CUSTOMER',   256);  //消费者     查看订单列表
+define('UTID_PROVIDER',   1);     //供货商
+define('UTID_PROSECOND',  2);     //二级商
+define('UTID_PURCHASER',  4);     //采购商
+define('UTID_BEIZHU',      16);    //贝竹总部
+define('UTID_MANAGER_L1', 64);    //一级代理 收一级代理费
+define('UTID_MANAGER_L2', 256);   //二级代理 收二级代理费
+define('UTID_GROWTH',      1024);  //发展商    收发展费
+define('UTID_SELLER',      4096);  //分销商    收销售费
+define('UTID_CUSTOMER',   16384); //消费者     查看订单列表
 
 //资金类型(type)
 define('PAY',             1);    //支付
@@ -59,8 +55,6 @@ define('VIR_RECH',        16);   //虚拟充值 virtual recharge
 define('REBUT',           32);   //提现驳回 rebut
 define('DFPROFIT',        64);   //到付反利
 define('ZFPROFIT',        128);  //在线利润
-
-
 
 //account_type
 define('ACCOUNT_TYPE_CARRY',      1); //可提现
